5b67917513819296b1602ae0bd82e49252b253fc
Cheng Zhan <chengzhanxd@gmail.com>
2024-07-31 18:29:26 +0800

    fix bug, middleware only asks for authentication for POST/DELETE now

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -27,12 +27,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.use(
-	"/api/blogs",
-	middleware.tokenExtractor,
-	middleware.userExtractor,
-	blogsRouter
-);
+app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
 
--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,58 +1,68 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blogDB");
-const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const middleware = require("../utils/middleware");
 
 blogsRouter.get("/", async (request, response) => {
 	const allBlogs = await Blog.find({}).populate("user", { blogs: 0 });
 	response.json(allBlogs);
 });
 
-blogsRouter.post("/", async (request, response) => {
-	const blog = request.body;
+blogsRouter.post(
+	"/",
+	middleware.tokenExtractor,
+	middleware.userExtractor,
+	async (request, response) => {
+		const blog = request.body;
 
-	if (!blog.title || !blog.url) {
-		return response
-			.status(400)
-			.json({ error: "title or url properties are missing" });
+		if (!blog.title || !blog.url) {
+			return response
+				.status(400)
+				.json({ error: "title or url properties are missing" });
+		}
+
+		const user = request.user;
+
+		const newBlog = new Blog({
+			title: blog.title,
+			author: blog.author || "Unknown Author",
+			url: blog.url,
+			likes: blog.likes,
+			user: user.id,
+		});
+
+		const savedBlog = await newBlog.save();
+		user.blogs = user.blogs.concat(newBlog._id);
+		await user.save();
+
+		response.status(201).json(savedBlog);
 	}
+);
 
-	const user = request.user;
+blogsRouter.delete(
+	"/:id",
+	middleware.tokenExtractor,
+	middleware.userExtractor,
+	async (request, response) => {
+		const id = request.params.id;
 
-	const newBlog = new Blog({
-		title: blog.title,
-		author: blog.author || "Unknown Author",
-		url: blog.url,
-		likes: blog.likes,
-		user: user.id,
-	});
+		const decodedToken = jwt.verify(request.token, process.env.SECRET);
+		if (!decodedToken.id) {
+			return response.status(401).json({ error: "token invalid" });
+		}
 
-	const savedBlog = await newBlog.save();
-	user.blogs = user.blogs.concat(newBlog._id);
-	await user.save();
+		const user = request.user;
 
-	response.status(201).json(savedBlog);
-});
+		user.blogs = user.blogs.filter(
+			(blogId) => blogId.toString() !== id.toString()
+		);
 
-blogsRouter.delete("/:id", async (request, response) => {
-	const id = request.params.id;
+		await user.save();
 
-	const decodedToken = jwt.verify(request.token, process.env.SECRET);
-	if (!decodedToken.id) {
-		return response.status(401).json({ error: "token invalid" });
+		await Blog.findByIdAndDelete(id);
+		return response.status(204).end();
 	}
-
-	const user = request.user;
-
-	user.blogs = user.blogs.filter(
-		(blogId) => blogId.toString() !== id.toString()
-	);
-
-	await user.save();
-
-	await Blog.findByIdAndDelete(id);
-	return response.status(204).end();
-});
+);
 
 blogsRouter.put("/:id", async (request, response) => {
 	const id = request.params.id;
