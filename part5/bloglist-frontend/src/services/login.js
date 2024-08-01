5a0dd8ae81ceea8cbf3569e31d85e81707bf40ec
Cheng Zhan <chengzhanxd@gmail.com>
2024-08-01 17:51:03 +0800

    add route for fetching login details

---
+++ part5/bloglist-frontend/src/services/login.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const baseUrl = "api/login";
+
+const login = ({ username, password }) => {
+	const request = axios.post(baseUrl, { username, password });
+	return request.then((response) => {
+		return response.data;
+	});
+};
+
+export default { login };
