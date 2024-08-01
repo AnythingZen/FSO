462f7c4dad8eca08d2900244ad86cb0548ef58cd
Cheng Zhan <chengzhanxd@gmail.com>
2024-08-01 22:20:09 +0800

    add message to display for successful creation or error

---
+++ part5/bloglist-frontend/src/components/DisplayMessage.jsx
@@ -0,0 +1,27 @@
+const DisplayMessage = ({ message }) => {
+	const addBlogStyle = {
+		color: "green",
+		border: "3px solid green",
+		borderRadius: "12px",
+		padding: "30px 20px",
+	};
+	const errorStyle = {
+		color: "red",
+		border: "3px solid red",
+		borderRadius: "12px",
+		padding: "30px 20px",
+	};
+	if (!message) {
+		return;
+	}
+
+	return message.author ? (
+		<h2 style={addBlogStyle}>
+			{message.title} {message.author} added
+		</h2>
+	) : (
+		<h2 style={errorStyle}>{message}</h2>
+	);
+};
+
+export default DisplayMessage;
