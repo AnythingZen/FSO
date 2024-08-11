54b67c667cde3fd1dabf18208c0f5ce246f29cdb
Cheng Zhan <chengzhanxd@gmail.com>
2024-08-11 23:36:46 +0800

    update tests to handle blog creation, likes, deleting and sorting of blogs based on likes

--- part5/playwright/tests/blog_app.spec.js
+++ part5/playwright/tests/blog_app.spec.js
@@ -1,6 +1,11 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
-const { login } = require("./helper");
-const { before } = require("node:test");
+const {
+	login,
+	createBlog,
+	toggleOpenBlog,
+	handleLikeButton,
+	toggleOpenMultipleBlogs,
+} = require("./helper");
 
 describe("Blog app", () => {
 	beforeEach(async ({ page, request }) => {
@@ -60,15 +65,170 @@ describe("Blog app", () => {
 	});
 
 	describe("After login", () => {
-		beforeEach(() => {
+		beforeEach(({ page }) => {
 			login(page, "PasswordIsUser123", "User123");
 		});
-		test("able to create a new blog", async ({ page }) => {
-            
-        });
 
-        test("blog can be seen after creating", async ({page}) => {
+		test("new blog created displays green message", async ({ page }) => {
+			await createBlog(
+				page,
+				"Canonical string reduction",
+				"Edsger W. Dijkstra",
+				"http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html"
+			);
+			const successfulBlogAddedMessage = page.getByRole("heading", {
+				name: "Canonical string reduction Edsger W. Dijkstra",
+			});
+			await expect(successfulBlogAddedMessage).toBeVisible();
+			await expect(successfulBlogAddedMessage).toHaveCSS(
+				"border",
+				"3px solid rgb(0, 128, 0)"
+			);
+		});
 
-        })
+		describe("New blog", () => {
+			beforeEach(async ({ page }) => {
+				await createBlog(
+					page,
+					"First class tests",
+					"Robert C. Martin",
+					"http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html"
+				);
+				await toggleOpenBlog(
+					page,
+					/^First class tests Robert C\. MartinView$/
+				);
+			});
+
+			test("can toggle open new blog", async ({ page }) => {
+				await expect(
+					page.getByRole("button", { name: "Remove" })
+				).toBeVisible();
+			});
+
+			test("increases like counts upon pressing", async ({ page }) => {
+				const likeButton = page.getByTestId("likeButton");
+				await expect(likeButton).toBeVisible();
+
+				await expect(page.getByText("0 like")).toBeVisible();
+				await likeButton.click();
+				await expect(page.getByText("1 like")).toBeVisible();
+			});
+
+			test("can be deleted", async ({ page }) => {
+				const blogToBeDeleted = page.getByText(
+					"First class testsHideRobert C"
+				);
+				await expect(blogToBeDeleted).toBeVisible();
+				// Setup listener for the window.confirm dialog
+				page.once("dialog", (dialog) => {
+					dialog.accept();
+				});
+				await page.getByRole("button", { name: "Remove" }).click();
+				await expect(blogToBeDeleted).not.toBeVisible();
+			});
+
+			test("can only be seen by user created", async ({
+				page,
+				request,
+			}) => {
+				await request.post("/api/users/", {
+					data: {
+						name: "NewUser",
+						username: "NewUserPassword12345",
+						password: "12345",
+					},
+				});
+				const blogCreatedByRoot = page.getByText(
+					"First class testsHideRobert C"
+				);
+				await expect(blogCreatedByRoot).toBeVisible();
+
+				await page.getByRole("button", { name: "Logout" }).click();
+
+				const header = page.getByRole("heading", {
+					name: "Log into application",
+				});
+				await expect(header).toBeVisible();
+
+				await login(page, "NewUserPassword12345", "12345");
+
+				const successfulLoginMessage = page.getByRole("heading", {
+					name: "NewUser logged in",
+				});
+				await expect(successfulLoginMessage).toBeVisible();
+				await expect(blogCreatedByRoot).not.toBeVisible();
+			});
+		});
+
+		describe("Creating multiple new blogs", () => {
+			beforeEach(async ({ page }) => {
+				await createBlog(
+					page,
+					"First class tests",
+					"Robert C. Martin",
+					"http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html"
+				);
+				await createBlog(
+					page,
+					"React patterns",
+					"Michael Chan",
+					"https://reactpatterns.com/"
+				);
+				await createBlog(
+					page,
+					"Canonical string reduction",
+					"Edsger W. Dijkstra",
+					"http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html"
+				);
+			});
+
+			test("is successful", async ({ page }) => {
+				await expect(
+					page.getByText("First class tests Robert C.")
+				).toBeVisible();
+				await expect(
+					page.getByText("React patterns Michael")
+				).toBeVisible();
+				await expect(
+					page.getByText(
+						"Canonical string reduction Edsger W. DijkstraView"
+					)
+				).toBeVisible();
+			});
+
+			test("blogs are sorted by like counts", async ({ page }) => {
+				await toggleOpenMultipleBlogs(page);
+				const oneLikeBlog = page.getByTestId("likeButton").first();
+				await handleLikeButton(oneLikeBlog, 1);
+
+				const twoLikesBlog = page
+					.getByText("React patternsHideMichael")
+					.getByTestId("likeButton");
+				await handleLikeButton(twoLikesBlog, 2);
+
+				const threeLikesBlog = page.getByTestId("likeButton").nth(2);
+				await handleLikeButton(threeLikesBlog, 3);
+
+				await page.reload();
+				await toggleOpenMultipleBlogs(page);
+				const blogElements = page.getByTestId("blogContent");
+
+				// Extract the like counts from each blog element
+				const likeCounts = await blogElements.evaluateAll((blogDivs) =>
+					blogDivs.map((blog) =>
+						parseInt(
+							blog
+								.querySelector("div:nth-child(4)")
+								.textContent.trim()
+								.split(" ")[0]
+						)
+					)
+				);
+
+				// Check if the like counts are in descending order
+				expect(likeCounts).toEqual(likeCounts.sort((a, b) => b - a));
+			});
+		});
 	});
 });
--- part5/playwright/tests/helper.js
+++ part5/playwright/tests/helper.js
@@ -7,4 +7,47 @@ const login = async (page, username, password) => {
 	await page.getByRole("button", { name: "Login" }).click();
 };
 
-module.exports = { login };
+const createBlogHelper = async (page, blogType, blogDesc) => {
+	const input = await page.getByPlaceholder(blogType);
+	await input.click();
+	await input.fill(blogDesc);
+};
+
+const createBlog = async (page, blogTitleDesc, blogAuthorDesc, blogUrlDesc) => {
+	await page.getByTestId("toggleButton").click();
+
+	await createBlogHelper(page, "title", blogTitleDesc);
+	await createBlogHelper(page, "author", blogAuthorDesc);
+	await createBlogHelper(page, "url", blogUrlDesc);
+
+	await page.getByTestId("createBlog").click();
+
+	await page.getByText(`${blogTitleDesc} ${blogAuthorDesc} added`).waitFor();
+};
+
+const toggleOpenBlog = async (page, text) => {
+	await page
+		.locator("div")
+		.filter({
+			hasText: text,
+		})
+		.getByRole("button")
+		.click();
+};
+
+const handleLikeButton = async (page, timesClicked) => {
+	for (let i = 0; i < timesClicked; i++) {
+		await page.click();
+	}
+};
+
+const toggleOpenMultipleBlogs = async (page) => {
+	await toggleOpenBlog(page, /^First class tests Robert C\. MartinView$/);
+	await toggleOpenBlog(page, /^React patterns Michael ChanView$/);
+	await toggleOpenBlog(
+		page,
+		/^Canonical string reduction Edsger W\. DijkstraView$/
+	);
+};
+
+module.exports = { login, createBlog, toggleOpenBlog, handleLikeButton, toggleOpenMultipleBlogs };
